f78349fa1e87ebdc81087d51e8bf9937f57966c8
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2020-10-24 12:47:23 -0400

    Init auth endpoint, db connection

---
+++ src/auth/auth-router.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const AuthService = require("./auth-service");
+const { requireAuth } = require('../middleware/jwt-auth')
+
+const authRouter = express.Router();
+const jsonBodyParser = express.json();
+
+// used for logging in users 
+
+authRouter.post("/login", jsonBodyParser, (req, res, next) => {
+  const { user_name, password } = req.body;
+  const loginUser = { user_name, password };
+  var errvar = '';
+
+  // if missing username or pass
+  for (const [key, value] of Object.entries(loginUser))
+    if (value == null) {
+     
+      if(key === user_name) errvar = "username."
+      else if(key === password) errvar = "password."
+
+      return res.status(400).json({
+        error: `Missing ${errvar}`,
+      });
+    }
+
+  AuthService.getUserWithUserName(req.app.get("db"), loginUser.user_name)
+    .then((dbUser) => {
+      if (!dbUser)
+        return res.status(400).json({
+          error: "User doesn't exist",
+        });
+
+      return AuthService.comparePasswords(
+        loginUser.password,
+        dbUser.password
+      ).then((compareMatch) => {
+        if (!compareMatch)
+          return res.status(400).json({
+            error: "Incorrect username or password",
+          });
+
+        const sub = dbUser.user_name;
+        const payload = { user_id: dbUser.id };
+        res.send({
+          user: {
+            type: dbUser.type,
+            user_id: dbUser.id 
+          },
+          authToken: AuthService.createJwt(sub, payload),
+        });
+      });
+    })
+    .catch(next);
+});
+
+module.exports = authRouter;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-  }
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/atomic',
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '59m',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || '*'
+}
